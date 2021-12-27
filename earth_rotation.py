b0bd26f9d72db4b63506b234fd54754d00c82483
Behrouz <behrouz.safari@gmail.com>
2021-12-28 00:37:20 +0100

    Update earth_rotation.py

--- earth_rotation.py
+++ earth_rotation.py
@@ -1,4 +1,4 @@
-# Number of points on Earth: 312
+# Number of points on Earth: n=300
 # Number of time moments: 96 (each 15 min in one day)
 
 import numpy as np
@@ -9,21 +9,18 @@ from astropy.time import Time
 from astropy.coordinates import EarthLocation, Longitude, Latitude
 from hypatie.animation import Body, play
 
+n = 300
 t0 = datetime(2022,1,1,0,0,0)
 times = [t0 + 0.25*timedelta(hours=i) for i in range(96)]
 ts = Time(times)
 
-lats = np.array([*range(-90, 105, 15)])
-lons = np.array([*range(-180, 180, 15)])
+# Choose random points on the surface of a sphere
+# https://mathworld.wolfram.com/SpherePointPicking.html
+rand1, rand2 = np.random.uniform(0,1,n), np.random.uniform(0,1,n)
+lons = np.array([(2*np.pi*u * (180/np.pi))-180 for u in rand1])
+lats = np.array([(np.arccos(2*v-1)*(180/np.pi)) - 90 for v in rand2])
 
-crd = []
-for y in lats:
-    for x in lons:
-        crd.append(np.array([y,x]))
-
-crd = np.array(crd)
-
-n = [str(i).zfill(3) for i in range(len(crd))]
+crd = np.array([*zip(lats,lons)])
 
 lats = crd[:,0]
 lons = crd[:,1]
@@ -39,8 +36,8 @@ locs = EarthLocation.from_geodetic(lon=lons, lat=lats)
 pos = np.array([locs.get_gcrs(i).cartesian.xyz.value for i in ts])
 print(pos.shape)
 
-bodies = [Body(n[i], pos[:,:,i], times) for i in range(len(locs))]
-names = n
+names = [str(i).zfill(3) for i in range(len(crd))]
+bodies = [Body(names[i], pos[:,:,i], times) for i in range(len(locs))]
 colors = ['blue']*len(locs)
 sizes = [1]*len(locs)
 
