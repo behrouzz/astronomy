a1856da22a8d46dd0f62dc202ed548ee9ff144fa
Behrouz <behrouz.safari@gmail.com>
2022-07-18 01:21:47 +0200

    Update 01.py

--- temperature/01.py
+++ temperature/01.py
@@ -1,28 +1,37 @@
+# Goto Gaia Tap service: https://gea.esac.esa.int/archive/
+# Run this script and download results as 'gaia_hip.csv'
 """
-SELECT Source, HIP, Gmag, BPmag, RPmag, Teff
-FROM "I/355/gaiadr3"
-WHERE (HIP IS NOT NULL)
-AND (Teff IS NOT NULL)
+SELECT 
+hip.source_id, 
+sl.parallax AS plx, 
+sl.phot_g_mean_mag AS g, 
+sl.phot_bp_mean_mag AS b, 
+sl.phot_rp_mean_mag AS r, 
+sl.teff_gspphot AS teff
+
+FROM gaiadr3.hipparcos2_best_neighbour as hip
+
+LEFT JOIN gaiadr3.gaia_source_lite AS sl
+ON hip.source_id=sl.source_id
+
+WHERE (hip.angular_distance < 0.005) AND (hip.number_of_neighbours=1) AND (sl.teff_gspphot IS NOT NULL)
 """
 
+import joblib
 import pandas as pd
-#from sklearn.linear_model import LinearRegression, SGDRegressor
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import train_test_split
 
 df = pd.read_csv('gaia_hip.csv')
 
-# blue, green, red
-df['G_B'] = df['Gmag'] - df['BPmag']
-df['R_G'] = df['RPmag'] - df['Gmag']
-df['R_B'] = df['RPmag'] - df['BPmag']
+df['G_B'] = df['g'] - df['b']
+df['R_G'] = df['r'] - df['g']
+df['R_B'] = df['r'] - df['b']
 
-#X = df[['Gmag', 'BPmag', 'RPmag']].values
-X = df[['G_B', 'R_G', 'R_B', 'Gmag', 'RPmag', 'BPmag']].values
-y = df['Teff'].values
+X = df[['G_B', 'R_G', 'R_B', 'g', 'r', 'b', 'plx']].values
+y = df['teff'].values
 
-X_train, X_test, y_train, y_test = train_test_split(
-    X, y, test_size=0.33, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X, y)
 
 model = RandomForestRegressor()
 
@@ -30,5 +39,6 @@ model.fit(X_train, y_train)
 
 score = model.score(X_test, y_test)
 print(score)
-# ['G_B', 'R_G', 'R_B', 'Gmag'] : 0.6447005820438643
-# ['G_B', 'R_G', 'R_B', 'Gmag', 'RPmag', 'BPmag']: 0.6690421583329298
+# 0.80
+
+joblib.dump(model, "teff_RF_model.joblib")
